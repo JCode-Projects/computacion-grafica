35337d3f4274c022fbd5c917d437b94dcc36073a
Yeferson Camilo Zaque <zaquecamilo@gmail.com>
2021-10-21 12:55:47 -0500

    Agregadas las graficas de lineas y circulos

---
+++ src/develop/DibujoCirculos.java
@@ -0,0 +1,117 @@
+package develop;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.Polygon;
+import javax.swing.JFrame;
+import java.awt.*;
+import static java.awt.Font.PLAIN;
+
+public class DibujoCirculos extends JFrame {
+    public DibujoCirculos() {
+        super("Dibujo Circulos");
+        setSize(460, 500);
+        setVisible(true);
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
+    
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        // Coordenadas del cuerpo y ojos de la abeja
+        int[][] puntos = {
+            {60, 200, 220, 135}, {240, 150, 80, 115},
+            {178, 152, 80, 115}, {260, 190, 35, 40}, 
+            {198, 190, 35, 40}
+        };
+
+        g2.setStroke(new BasicStroke(4.0f));
+        
+        // Alas de la abeja
+        g2.draw(new Ellipse2D.Double(100, 110, 60, 120));
+        g2.draw(new Ellipse2D.Double(160, 110, 60, 120));
+
+        g2.rotate(-0.077f);
+
+        // Dibujo del lapiz
+        // Borrador
+        g2.setColor(Color.getHSBColor(0.907f, 1, 0.73f));
+        g2.fillOval(43, 330, 60, 70);
+        g2.setColor(Color.BLACK);
+        g2.drawOval(43, 330, 60, 70);
+
+        // Metal del borrador
+        g2.setColor(Color.getHSBColor(0.107f, 0.2f, 0.73f));
+        g2.fillOval(63, 330, 20, 70);
+        g2.setColor(Color.BLACK);
+        g2.drawOval(63, 330, 20, 70);
+
+        // Cuerpo del lapiz
+        Polygon figura = new Polygon();
+        figura.addPoint(268, 330);
+        figura.addPoint(333, 360);
+        figura.addPoint(333, 370);
+        figura.addPoint(268, 400);
+
+        // Punta del lapiz
+        g2.setColor(Color.getHSBColor(0.107f, 0.2f, 0.73f));
+        g2.fillPolygon(figura);
+        g2.setColor(Color.BLACK);
+        g2.drawPolygon(figura);
+
+        // Cuerpo del lapiz
+        g2.setColor(Color.getHSBColor(0.16f, 0.9f, 0.9f));
+        g2.fillRoundRect(73, 330, 200, 70, 20, 20);
+        g2.setColor(Color.BLACK);
+        g2.drawRoundRect(73, 330, 200, 70, 20, 20);
+
+        // Color a toda la abeja
+        g2.setColor(Color.getHSBColor(0.16f, 0.9f, 0.9f));
+        g2.fillArc(60, 200, 220, 135, 10, 360);
+
+        // Primera franja negra
+        g2.setColor(Color.BLACK);
+        g2.fill(new Arc2D.Double(60, 200, 220, 135, 110, 140, Arc2D.CHORD));
+
+        // Primer franja amarilla
+        g2.setColor(Color.getHSBColor(0.16f, 0.9f, 0.9f));
+        g2.fill(new Arc2D.Double(100, 200, 120, 135, 110, 140, Arc2D.CHORD));
+
+        // Segunda franja negra
+        g2.setColor(Color.BLACK);
+        g2.fill(new Arc2D.Double(130, 200, 120, 135, 110, 360, Arc2D.CHORD));
+
+        // Segunda franja amarilla
+        g2.setColor(Color.getHSBColor(0.16f, 0.9f, 0.9f));
+        g2.fill(new Arc2D.Double(160, 206, 120, 120, 110, 360, Arc2D.CHORD));
+
+        // Dibujo del cuerpo y los ojos
+        for(int i = 0; i < puntos.length; i++) {
+            g2.setColor(Color.WHITE);
+            if(i != 0) {
+                if(i == 3 || i == 4) g2.setColor(Color.BLACK);
+                g2.fillOval(puntos[i][0], puntos[i][1], puntos[i][2], puntos[i][3]);
+            }
+                
+            g2.setColor(Color.BLACK);
+            g2.drawOval(puntos[i][0], puntos[i][1], puntos[i][2], puntos[i][3]);
+        }
+
+        g2.drawArc(190, 175, 80, 120, 230, 50);
+
+        g2.setColor(Color.BLACK);
+        g2.setFont(new Font("CENTURY GOTHIC", PLAIN,32));
+        g2.drawString("Camilo", 100, 380);
+    }
+    
+    public static void main(String[] args) {
+        DibujoCirculos appLines = new DibujoCirculos();
+        appLines.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ src/develop/DibujoLineas.java
@@ -0,0 +1,226 @@
+package develop;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+import javax.swing.JFrame;
+
+
+public class DibujoLineas extends JFrame {
+    public DibujoLineas() {
+        super("Dibujo Lineas");
+        setSize(360, 460);
+        setVisible(true);
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
+    
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        /* >>>>> DIBUJANDO LA LUNA <<<<< */
+        // Coordenadas para dibujar la bruja
+        int[][][] points = {
+            // Coordenadas Luna [0]
+            {
+                {250, 50}, {280, 50}, {300, 60},
+                {310, 80}, {310, 110}, {300, 130},
+                {280, 140}, {250, 140}, {270, 130},
+                {280, 110}, {280, 70}, {270, 60},
+                {250, 50}
+            },
+            // Coordenadas parte sombrero superior [1]
+            {
+                {130, 50}, {170, 80}, {150, 80},
+                {190, 160}, {70, 160}, {130, 50}
+            },
+            // Coordenadas parte sombrero inferior [2]
+            {
+                {30, 160}, {230, 160}, {230, 170}, 
+                {30, 170}, {30, 160}
+            },
+            // Cordenadas cinta negra sombrero [3]
+            {
+                {92, 120}, {170 ,120}, {180, 140},
+                {81, 140}, {92, 120}
+            },
+            // Coordenadas hebilla en la cinta negra [4]
+            {
+                {110, 110}, {150, 110}, {150, 150},
+                {110, 150}, {110, 110}
+            },
+            // Coordenadas parte interna de la hebilla [5]
+            {
+                {120, 120}, {140, 120}, {140, 140},
+                {120, 140}, {120, 120}
+            },
+            // Coodenadas del cabello en lado izquierdo [6]
+            {
+                {70,170}, {10, 230}, {20, 240},
+                {60, 200}, {30, 240}, {40, 250},
+                {66, 210}, {50, 260}, {60, 270},
+                {92, 170}, {65,170}
+            },
+            // Coordenadas del cabello en el lado derecho [7]
+            {
+                {190, 170}, {250, 230}, {240, 240},
+                {200, 200}, {230, 240}, {220, 250},
+                {195, 210}, {220, 260}, {210, 270},
+                {168 ,170}, {195, 170}
+            },
+            // Coordenadas de la cara [8]
+            {
+                {92, 170}, {168 ,170}, {193, 230},
+                {193, 250}, {163, 280}, {103, 280},
+                {73, 250}, {73, 230}, {92, 170}
+            },
+            // Coordenadas de la esclerótica del ojo izquierdo [9]
+            {
+                {93, 180}, {123, 180}, {123, 210},
+                {93, 210}, {93, 180}
+            },
+            // Coordenadas del iris del ojo izquierdo [10]
+            {
+                {103, 190}, {123, 190}, {123, 210}, 
+                {103, 210}, {103, 190}
+            },
+            // Coordenadas de la esclerótica del ojo derecho [11]
+            {
+                {140, 180}, {170, 180}, {170, 210},
+                {140, 210}, {140, 180}
+            },
+            // Coordenadas del iris del ojo derecho [12]
+            {
+                {140, 190}, {160, 190}, {160, 210},
+                {140, 210}, {140, 190}
+            },
+            // Coordenada de la nariz [13]
+            {
+                {160, 210}, {83, 210}, {83, 230},
+                {143, 230}
+            },
+            // Coordenadas de la boca [14]
+            {
+                {103, 240}, {103, 250}, {113, 260},
+                {153, 260}, {163, 250}, {163, 240}
+            },
+            // Coordenadas palo escoba [15]
+            {
+                {23, 370}, {243, 370}, {243, 380},
+                {23, 380}, {23, 370}
+            },
+            // Coordenadas escoba [16]
+            {
+                {243, 370}, {253, 340}, {343, 340},
+                {343, 410}, {253, 410}, {243, 380},
+                {243, 370}
+            },
+            // Coordenadas vestido [17]
+            {
+                {103, 280}, {163, 280}, {193, 370},
+                {193, 380}, {73, 380}, {73, 370},
+                {103, 280}
+            },
+            // Coordenadas brazo izquierdo [18]
+            {
+                {103, 280}, {43, 350}, {53, 360},
+                {92, 315}
+            },
+            // Coordenadas mano izquierda [19]
+            {
+                {43, 350}, {33, 360}, {33, 370}, 
+                {53, 370}, {53, 360}, {43, 350}
+            },
+            // Coordenadas brazo derecho [20]
+            {
+                {163, 280}, {233, 275}, {243, 285},
+                {173, 310}, {163, 280}
+            },
+            // Coordenadas mano derecha [21]
+            {
+                {233, 275}, {233, 255}, {243, 255},
+                {243, 265}, {253, 255}, {263, 275},
+                {243, 285}, {233, 275}
+            },
+            // Coordenadas pierna izquierda [22]
+            {
+                {93, 380}, {123, 420}, {163, 420},
+                {173, 440}, {173, 410}, {133, 410},
+                {117, 380}, {93, 380}
+            },
+            // Coordenadas media pie izquierdo [23]
+            {
+                {143, 420}, {153, 420}, {153, 410},
+                {143, 410}, {143, 420}
+            },
+            // Coordenadas bota pie izquierdo [24]
+            {
+                {153, 420}, {163, 420}, {173, 440}, 
+                {173, 410}, {153, 410}, {153, 420}
+            },
+            // Coordenadas pierna derecha [25]
+            {
+                {137, 380}, {157, 410}, {197, 410}, 
+                {207, 430}, {207, 400}, {167, 400},
+                {157, 380}, {137, 380}
+            }, 
+            // Coordenadas media pie derecho [26]
+            {
+                {177, 410}, {187, 410}, {187, 400},
+                {177, 400}, {177, 410}
+            }, 
+            // Coordenadas bota pie derecho [27]
+            {
+                {187, 410}, {197, 410}, {207, 430},
+                {207, 400}, {187, 400}, {187, 410}
+            }
+        };
+
+        // Ciclo para colorear las diferentes partes
+        for(int i = 0; i < points.length; i++) {
+            if(i != 13 && i != 14) {
+                Polygon figura = new Polygon();
+
+                for(int j = 0; j < points[i].length; j++) {
+                    figura.addPoint(points[i][j][0], points[i][j][1]);
+                }
+
+                if(i == 0 || i == 4 || i == 23 || i == 26) {
+                    g.setColor(Color.YELLOW);
+                } else if(i == 1 || i == 2 || i == 17 || i == 18 || i == 20) {
+                    g.setColor(Color.getHSBColor(2.80f, 0.92f, 0.76f));
+                } else if(i == 8 || i == 19 || i == 21 || i == 22 || i == 25) {
+                    g.setColor(Color.getHSBColor(1.29f, 0.45f, 0.80f));
+                } else if(i == 9 || i == 11) {
+                    g.setColor(Color.WHITE);
+                } else if(i == 15) {
+                    g.setColor(Color.getHSBColor(0.10f, 0.58f, 0.75f));
+                } else if(i == 16) {
+                    g.setColor(Color.getHSBColor(0.15f, 0.98f, 0.85f));
+                } else {
+                    g.setColor(Color.BLACK);
+                }
+
+                g.fillPolygon(figura);
+            }
+        }
+
+        // Ciclo para dibujar a lineas todo el contorno de la figura
+        g.setColor(Color.BLACK);
+        for(int i = 0; i < points.length; i++) {
+            for(int j = 0; j < points[i].length; j++) {
+                g.drawLine(
+                    points[i][j][0], 
+                    points[i][j][1], 
+                    points[i][(j < points[i].length - 1) ? j + 1 : j][0],
+                    points[i][(j < points[i].length - 1) ? j + 1 : j][1]
+                );
+            }
+        }
+    }
+    
+//    public static void main(String[] args) {
+//        DibujoLineas appLines = new DibujoLineas();
+//        appLines.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    }
+}
